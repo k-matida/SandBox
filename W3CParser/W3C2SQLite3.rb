49eb7ab26ad99c212bc4975f996db025b4a8a2b5
Kazuya Machida <k-matida@tsrweb.co.jp>
2012-11-12 15:57:20 +0900

    add yamlconf and ranking option

--- W3CParser/W3C2SQLite3.rb
+++ W3CParser/W3C2SQLite3.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env jruby
 # W3C2SQLite3.rb 0.0.1
 # テキスト形式のIIS(W3CFormat)ログをSQLite3に登録する
 #
@@ -10,60 +10,60 @@
 # なお、IIS W3C形式のログは、出力形式がカスタマイズ可能である。
 # そのためこのプログラムで処理できるフォーマットを明記する必要が
 # ある。サンプルでなければ。
-
-# 後でymlに移動するよ
-# データベース論理名
-DBNAME='/home/hoi/git/SandBox/W3CParser/pom.sqlite3'
-DBURL='DBI:jdbc:sqlite:' + DBNAME
-DBUSER= nil
-DBPASSWORD= nil
-JDBCDRIVER= 'org.sqlite.JDBC'
+require 'yaml'
+config = YAML.load_file('W3CPConfig.yml')
+DBNAME = config['DBNAME']
+DBURL = config['DBURLPREFIX'] + DBNAME 
+DBUSER = config['DBUSER']
+DBPASSWORD = config['DBPASSWORD'] 
+JDBCDRIVER = config['JDBCDRIVER']
 
 unless ARGV[0]
   puts "Usage: [TODO]"
   exit 1
 end
 
+#
 # W3C用のログテーブルにアクセスする。
+#
 require 'rubygems'
 require 'java'
 require 'dbi'
-# dbd-jbdc 0.1.4 以外はdbd/jdbcのようで嵌った
-require 'dbd/Jdbc'
+require 'dbd/Jdbc' # dbd-jbdc 0.1.4 以外はdbd/jdbcのようで嵌った
 require 'jdbc/sqlite3'
 
 Dir.chdir(File.dirname(File.expand_path(DBNAME)))
-dbh = DBI.connect(DBURL, DBUSER, DBPASSWORD, 'AutoCommit'=>false, 'driver'=>JDBCDRIVER)
+DBI.connect(DBURL, DBUSER, DBPASSWORD, 'AutoCommit'=>false, 'driver'=>JDBCDRIVER) do |dbh|
 
-# SQL文の管理は本当なら別の機構が必要だろう
-sql = <<SQL
-SELECT tbl_name
-FROM sqlite_master
-WHERE type=='table'
+  # SQL文の管理は本当なら別の機構が必要だろう
+  sql = <<SQL
+  SELECT tbl_name
+  FROM sqlite_master
+  WHERE type=='table'
 SQL
 
-unless(dbh.execute(sql).fetch_all.include?(["log"]))
-sql = <<SQL
-CREATE TABLE log(
-  c_ip,
-  date,
-  time, 
-  s_computername, 
-  time_taken,
-  sc_bytes,
-  cs_bytes,
-  sc_status,
-  sc_win32_status,
-  cs_method, 
-  cs_uri_query,
-  cs_u_a
-  );"
+  unless(dbh.execute(sql).fetch_all.include?(["log"]))
+  sql = <<SQL
+  CREATE TABLE log(
+    c_ip,
+    date,
+    time, 
+    s_computername, 
+    time_taken,
+    sc_bytes,
+    cs_bytes,
+    sc_status,
+    sc_win32_status,
+    cs_method, 
+    cs_uri_query,
+    cs_u_a
+    );"
 SQL
-  dbh.do(sql)
-end
+    dbh.do(sql)
+  end
 
-query_insert= <<SQL
-INSERT INTO log VALUES(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12)
+  query_insert= <<SQL
+  INSERT INTO log VALUES(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12)
 SQL
 #  :c_ip,
 #  :date,
@@ -79,16 +79,18 @@ SQL
 #  :cs_u_a
 #  );"
 #SQL
-# ログをインサートするsqlを準備
-st=dbh.prepare(query_insert)
+  # ログをインサートするsqlを準備
+  st=dbh.prepare(query_insert)
 
-# pg Read W3C Format file 
-# 加工してインポートしたほうがよさそうだけど学習のため一行ずつ処理
-require 'csv'
-CSV.open(ARGV[0], 'r') do |i|
-  args=i.values_at(0,2,3,5,7,8,9,10,11,12,13).map{|v| v.strip}
-  st.execute(*(args + [nil]))
+  #
+  # IISログを読み込んでテーブルに挿入する。
+  # 加工してインポートしたほうがよさそうだけど学習のため一行ずつ処理
+  # 
+  require 'csv'
+  CSV.open(ARGV[0], 'r') do |i|
+    args=i.values_at(0,2,3,5,7,8,9,10,11,12,13).map{|v| v.strip}
+    st.execute(*(args + [nil]))
+  end
+
+  dbh.commit
 end
-
-dbh.commit
-dbh.disconnect
--- W3CParser/W3CParser.rb
+++ W3CParser/W3CParser.rb
@@ -1,49 +1,59 @@
-#!/usr/bin/env ruby
-# W3CParser.rb 0.0.1
-# 
-# 応答がN秒以上かかった画面のランキング上位M件を表示する。
-#
-# Usage: ruby W3CParser [todo]
+#!/usr/bin/env jruby
+# W3CParser.rb
+# 応答がt秒以上かかった画面のランキング上位n件を表示する。
 #
 
-# 後でymlに移動するよ
-# データベース論理名
-DBNAME='/home/hoi/git/SandBox/W3CParser/pom.sqlite3'
-DBURL='DBI:jdbc:sqlite:' + DBNAME
-DBUSER= nil
-DBPASSWORD= nil
-JDBCDRIVER= 'org.sqlite.JDBC'
+# option解析
+require 'optparse'
+OptionParser.new do |opt|
+  # バージョン情報
+  Version = "0.1.0"
+  opt.on('-t SECONDS[-]', '--time SECONDS[-]', 'かかった時間SECONDS以上を検索対象とする。既定値 10秒') {|v| @TIME = v }
+  opt.on('-n COUNT[-]', '--lines COUNT[-]', '上位COUNT件数を表示する。既定値 10件') {|v| @LINES = v }
+  opt.on('-d DATE[-]', '--date DATE[-]', '対象の日付を選択する。format mm/dd/yyyy。省略時は実行日') {|v| @DATE = v }
+  opt.parse!
+  # Defalut value
+  @TIME ||= "10"
+  @LINES ||= "10"
+  @DATE ||= `date "+%m/%d/%Y"` 
+end 
+
+# configファイルの読込
+require 'yaml'
+config = YAML.load_file('W3CPConfig.yml')
+DBNAME = config['DBNAME']
+DBURL = config['DBURLPREFIX'] + config['DBNAME']
+DBUSER = config['DBUSER']
+DBPASSWORD = config['DBPASSWORD'] 
+JDBCDRIVER = config['JDBCDRIVER']
 
 # W3C用のログテーブルにアクセスする。
 require 'rubygems'
 require 'java'
 require 'dbi'
-# dbd-jbdc 0.1.4 以外はdbd/jdbcのようで嵌った
-require 'dbd/Jdbc'
+require 'dbd/Jdbc' # dbd-jbdc 0.1.4 以外はdbd/jdbcのようで嵌った
 require 'jdbc/sqlite3'
 
 Dir.chdir(File.dirname(File.expand_path(DBNAME)))
-dbh = DBI.connect(DBURL, DBUSER, DBPASSWORD, 'AutoCommit'=>false, 'driver'=>JDBCDRIVER)
-
-sql=<<SQL
-SELECT
-  cs_uri_query
-  , count(*) as cnt
-FROM
-  log
-GROUP BY
-  cs_uri_query
-ORDER BY
-  cnt desc
-LIMIT 10
+DBI.connect(DBURL, DBUSER, DBPASSWORD, 'AutoCommit'=>false, 'driver'=>JDBCDRIVER) do |dbh|
+  query = <<SQL
+  SELECT
+    cs_uri_query
+    , count(*) as cnt
+  FROM
+    log
+  WHERE 
+    abs(time_taken) > (:1 * 1000) 
+    AND date = :2
+  GROUP BY
+    cs_uri_query
+  ORDER BY
+    cnt desc
+  LIMIT :3 
 SQL
-
-puts "%-40s %s"%['画面名','件数']
-
-log_table = dbh.execute(sql)
-log_table.fetch do |row|
-  puts "%-37s %d"%[row[0],row[1]]
+  puts "%-40s %s"%['画面名','件数']
+  dbh.execute(query, @TIME, @DATE, @LINES ).fetch do |row|
+    puts "%-37s %d"%[row[0],row[1]]
+  end
+  dbh.commit
 end
-
-dbh.commit
-dbh.disconnect
