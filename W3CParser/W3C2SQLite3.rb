3ed52b6f0c681597c19111c50957e6b506fbca13
Kazuya Machida <k-matida@tsrweb.co.jp>
2012-11-13 18:28:29 +0900

    Although W3CParser was class-ized, it does not understand well.

--- W3CParser/W3C2SQLite3.rb
+++ W3CParser/W3C2SQLite3.rb
@@ -14,26 +14,20 @@ require 'active_record'
 class LogTableMapper 
 
   def initialize (config_file_name)
-    config = YAML.load_file(config_file_name)
-    @dbname = config["DBNAME"] 
-    @adapter = config["ADAPTER"]
     #ActiveRecord::Base.logger = Logger.new("debug.log")
-  end
-
-  def connect
+    config = YAML.load_file(config_file_name)
     ActiveRecord::Base.establish_connection(
-      :adapter => @adapter,
-      :database => @dbname
+      :adapter => config["ADAPTER"],
+      :database => config["DBNAME"]
     )
   end
 
   def insertlog csv_file_name
-    self.connect
     Log.transaction do
       CSV.foreach(csv_file_name) do |row|
         log = Log.new
         log.c_ip = row[0]
-        log.date = row[2]
+        log.date = row[2].strip
         log.time = row[3]
         log.s_computername = row[5]
         log.time_taken = row[7]
@@ -42,7 +36,7 @@ class LogTableMapper
         log.sc_status = row[10]
         log.sc_win32_status = row[11]
         log.cs_method = row[12]
-        log.cs_uri_stem = row[13]
+        log.cs_uri_stem = row[13].strip
         log.save
       end
     end 
--- W3CParser/W3CParser.rb
+++ W3CParser/W3CParser.rb
@@ -1,59 +1,60 @@
 #!/usr/bin/env jruby
-# W3CParser.rb
+#
 # 応答がt秒以上かかった画面のランキング上位n件を表示する。
 #
-
-# option解析
 require 'optparse'
-OptionParser.new do |opt|
-  # バージョン情報
-  Version = "0.1.0"
-  opt.on('-t SECONDS[-]', '--time SECONDS[-]', 'かかった時間SECONDS以上を検索対象とする。既定値 10秒') {|v| @TIME = v }
-  opt.on('-n COUNT[-]', '--lines COUNT[-]', '上位COUNT件数を表示する。既定値 10件') {|v| @LINES = v }
-  opt.on('-d DATE[-]', '--date DATE[-]', '対象の日付を選択する。format mm/dd/yyyy。省略時は実行日') {|v| @DATE = v }
-  opt.parse!
-  # Defalut value
-  @TIME ||= "10"
-  @LINES ||= "10"
-  @DATE ||= `date "+%m/%d/%Y"` 
-end 
-
-# configファイルの読込
 require 'yaml'
-config = YAML.load_file('database.yml')
-DBNAME = config['DBNAME']
-DBURL = config['DBURLPREFIX'] + config['DBNAME']
-DBUSER = config['DBUSER']
-DBPASSWORD = config['DBPASSWORD'] 
-JDBCDRIVER = config['JDBCDRIVER']
-
-# W3C用のログテーブルにアクセスする。
 require 'rubygems'
-require 'java'
-require 'dbi'
-require 'dbd/Jdbc' # dbd-jbdc 0.1.4 以外はdbd/jdbcのようで嵌った
-require 'jdbc/sqlite3'
+require 'active_record'
 
-Dir.chdir(File.dirname(File.expand_path(DBNAME)))
-DBI.connect(DBURL, DBUSER, DBPASSWORD, 'AutoCommit'=>false, 'driver'=>JDBCDRIVER) do |dbh|
-  query = <<SQL
-  SELECT
-    cs_uri_query
-    , count(*) as cnt
-  FROM
-    log
-  WHERE 
-    abs(time_taken) > (:1 * 1000) 
-    AND date = :2
-  GROUP BY
-    cs_uri_query
-  ORDER BY
-    cnt desc
-  LIMIT :3 
-SQL
-  puts "%-40s %s"%['画面名','件数']
-  dbh.execute(query, @TIME, @DATE, @LINES ).fetch do |row|
-    puts "%-37s %d"%[row[0],row[1]]
+class LogTableAttr
+  def initialize(config_file_name)
+    ActiveRecord::Base.logger = Logger.new('debug.log')
+    config = YAML.load_file('database.yml')
+    ActiveRecord::Base.establish_connection(
+      :adapter => config['ADAPTER'],
+      :database => config['DBNAME']
+    )
   end
-  dbh.commit
+
+  def select(time, date, line)
+    puts "%-40s %s"%['画面名','件数']
+    Log.find(
+      :all,
+      :select => "cs_uri_stem, count(*) as cnt",
+      :conditions => ['abs(time_taken) > (:time * 1000 ) AND date = :date', {:time => time, :date => date }],
+      :order => 'cnt desc',
+      :group => 'cs_uri_stem',
+      :limit => line
+    ).each do |row|
+      puts "%-37s %d"%[row.cs_uri_stem,row.cnt]
+    end
+  end
 end
+
+class Log < ActiveRecord::Base
+end
+
+class W3CParser
+  def main
+    args_parse
+    LogTableAttr.new('database.yml').select(@TIME, @DATE, @LINES)
+  end
+
+  private 
+  def args_parse
+    OptionParser.new do |opt|
+      opt.version = "0.1.0"
+      opt.on('-t SECONDS[-]', '--time SECONDS[-]', 'かかった時間SECONDS以上を検索対象とする。既定値 10秒') {|v| @TIME = v }
+      opt.on('-n COUNT[-]', '--lines COUNT[-]', '上位COUNT件数を表示する。既定値 10件') {|v| @LINES = v }
+      opt.on('-d DATE[-]', '--date DATE[-]', '対象の日付を選択する。format m/d/yyyy。省略時は実行日') {|v| @DATE = v }
+      opt.parse!
+      # Defalut value
+      @TIME ||= "10"
+      @LINES ||= "10"
+      @DATE ||= `date "+%m/%d/%Y"` 
+    end 
+  end
+end
+
+W3CParser.new.main
